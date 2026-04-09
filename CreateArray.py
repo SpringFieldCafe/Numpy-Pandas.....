7bb210897a8ab633a792b561625d9f3b483cf925
綾波あやなみ <2574703489@qq.com>
2026-04-09 15:33:27 +0800

    complete CreateArray.py

---
+++ CreateArray.py
@@ -0,0 +1,13 @@
+from numpy import *
+A=array([2,23,4])
+print(A)
+print(type(A))
+B=array([4,3,4],dtype=float)
+print(B.dtype)
+c=69999999999999999999999999999999999999999999999999999999.743897467382291
+C=array([c,12,3],dtype=float)
+D=array([c,123,56,2])
+print(C,D)
+E=array([[23,345,66],
+        [68,34,134]],dtype=float)
+print(E)
