f03306d6dcef2aa609522be67dd56701071f6015
綾波あやなみ <2574703489@qq.com>
2026-04-12 20:25:22 +0800

    add Index.py

---
+++ Index.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+A=np.arange(3,15)
+print(A)
+print(A[3])
+B=A.reshape(3,4)
+print(B,A)
+print(B[2])
+print(B[2][1])
+print(B[2,1])
+print(B[2,1:3])
+print(B[:,1])
+
+for row in B:
+    print(row)
+
+for column in B.T:
+    print(column)
+
+for item in B.flat:
+    print(item)
